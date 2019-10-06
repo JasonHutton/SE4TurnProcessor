f61146594977c59af9278a0677ff11d67e7582df
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2019-10-06 06:53:27 -0700

    Window name.

--- SE4TurnProcessor/Form1.Designer.cs
+++ SE4TurnProcessor/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.lblSavePath = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.txtSavePath = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -66,6 +67,8 @@
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.Controls.Add(this.txtSavePath);
             this.panel1.Controls.Add(this.lblBinPath);
             this.panel1.Controls.Add(this.txtBinPath);
@@ -85,14 +88,24 @@
             this.txtSavePath.TabIndex = 3;
             this.txtSavePath.TextChanged += new System.EventHandler(this.txtSavePath_TextChanged);
             // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Location = new System.Drawing.Point(13, 86);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(775, 100);
+            this.panel2.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SE4 Turn Processor";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -106,6 +119,7 @@
         private System.Windows.Forms.Label lblSavePath;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox txtSavePath;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
